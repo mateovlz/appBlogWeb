af67d62bd9040f08f654098a2655ef005e2c0201
Mateo <mateoande-99@hotmail.com>
2018-12-13 21:45:33 -0500

    CRU listo falta opcion de Delete

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render , get_object_or_404
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.shortcuts import redirect
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('pk')
     return render(request,'blog/post_list.html',{'posts': posts})
 
 
@@ -14,5 +15,27 @@ def post_detail(request, pk):
     return render(request,'blog/post_detail.html',{'post': post})
 
 def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html',{'form': form})
+    form = PostForm(request.POST)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request,'blog/post_edit.html',{'form':form})
+
+def post_edit(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request,'blog/post_edit.html',{'form': form})
