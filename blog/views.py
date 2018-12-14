a05128d46dcd6d4bb6a08adc5b97e9e1e47bf145
Mateo <mateoande-99@hotmail.com>
2018-12-13 21:07:44 -0500

    Plantilla para crear post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 
 def post_list(request):
@@ -11,3 +12,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html',{'form': form})
